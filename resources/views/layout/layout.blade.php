d4ea97b4278a15bdf19130891f9813023f2e99cb
nurrohman <nurrohman@javan.co.id>
2016-07-26 11:03:51 +0700

    test

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Gentellela Alela! | </title>
+    <title>Gentellela Alela! | | </title>
 
     <!-- Bootstrap -->
     <link href="{!! asset('gentelella/plugin/bootstrap/dist/css/bootstrap.min.css') !!}" rel="stylesheet">
